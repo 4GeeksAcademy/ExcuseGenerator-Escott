f11bcf735653066cdfe25b497db99cdae103d085
EScott237 <143569907+EScott237@users.noreply.github.com>
2023-10-05 03:05:39 +0000

    Erika's update

--- src/app.js
+++ src/app.js
@@ -8,4 +8,32 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("#the-excuse").innerHTML = excuseGenerator();
+};
+
+// Who, What, Where, When, Why, & How
+let excuseGenerator = () => {
+  let article = ["A", "The"];
+  let subject = ["burglar", "cat", "dog", "monkey", "donkey"];
+  let action = ["stole my", "slapped my", "punched my", "threw my"];
+  let possession = ["homework", "keys", "laptop", "phone"];
+  let where = ["at my house", "in my car", "on the way", "at the park"];
+
+  let articleIndex = Math.floor(Math.random() * article.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possessionIndex = Math.floor(Math.random() * possession.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return [
+    article[articleIndex] +
+      " " +
+      subject[subjectIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      possession[possessionIndex] +
+      " " +
+      where[whereIndex]
+  ];
 };
